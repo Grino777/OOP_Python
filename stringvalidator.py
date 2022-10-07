7abe34326204df1b8f354e148633501187a1867f
Grino777 <bars_B_P@mail.ru>
2022-10-07 13:28:46 +0300

    Finished StringValidator

---
+++ stringvalidator.py
@@ -0,0 +1,44 @@
+class StringValue:
+    """Дескриптор"""
+
+    def __init__(self, validator):
+        self.validator = validator
+
+    def __set_name__(self, owner, name):
+        self.name = '_' + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        if self.validator.validate(value):
+            setattr(instance, self.name, value)
+
+class ValidateString:
+    def __init__(self, min_length=3, max_length=100):
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def validate(self, string):
+        return isinstance(string, str) and self.min_length <= len(string) <= self.max_length
+
+
+class RegisterForm:
+    login = StringValue(ValidateString())
+    password = StringValue(ValidateString())
+    email = StringValue(ValidateString())
+
+    def __init__(self, login, password, email):
+        self.login = login
+        self.password = password
+        self.email = email
+
+    def get_fields(self):
+        return [self.login, self.password, self.email]
+
+    def show(self):
+        return f"<form>\
+                Логин: {self.login}\
+                Пароль: {self.password}\
+                Email: {self.email}\
+                </form>"
