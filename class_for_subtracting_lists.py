ad035ea88d4063d06cb62d9eec1f5aa1fc97e2e7
Grino777 <bars_B_P@mail.ru>
2022-11-03 12:54:27 +0300

    Finished class_for_subtracting_lists.py

---
+++ class_for_subtracting_lists.py
@@ -0,0 +1,94 @@
+"""
+Известно, что в Python мы можем соединять два списка между собой с помощью оператора +:
+
+lst = [1, 2, 3] + [4.5, -3.6, 0.78]
+Но нет реализации оператора -, который бы убирал из списка соответствующие значения вычитаемого списка, как это показано в примере:
+
+lst = [1, 2, 3, 4, 5, 6] - [5, 6, 7, 8, 1] # [2, 3, 4] (порядок следования оставшихся элементов списка должен сохраняться)
+Давайте это поправим и создадим такой функционал. Для этого нужно объявить класс с именем NewList, объекты которого создаются командами:
+
+lst = NewList() # пустой список
+lst = NewList([-1, 0, 7.56, True]) # список с начальными значениями
+Реализуйте для этого класса работу с оператором вычитания, чтобы над объектами класса NewList можно было выполнять следующие действия:
+
+lst1 = NewList([1, 2, -4, 6, 10, 11, 15, False, True])
+lst2 = NewList([0, 1, 2, 3, True])
+res_1 = lst1 - lst2 # NewList: [-4, 6, 10, 11, 15, False]
+lst1 -= lst2 # NewList: [-4, 6, 10, 11, 15, False]
+res_2 = lst2 - [0, True] # NewList: [1, 2, 3]
+res_3 = [1, 2, 3, 4.5] - res_2 # NewList: [4.5]
+a = NewList([2, 3])
+res_4 = [1, 2, 2, 3] - a # NewList: [1, 2]
+Также в классе NewList необходимо объявить метод:
+
+get_list() - для возвращения результирующего списка объекта класса NewList
+
+Например:
+
+lst = res_2.get_list() # [1, 2, 3]
+"""
+
+
+class NewList:
+    def __init__(self, *args):
+        if not args:
+            self.new_list = []
+        else:
+            self.new_list = args[0]
+
+    def __sub__(self, other):
+        l1, l2 = list(sorted([self, self.check_type_other(other)], key=len, reverse=True))
+        l1 = [(i, type(i)) for i in l1.new_list]
+        l2 = [(i, type(i)) for i in l2.new_list]
+        res = l1.copy()
+        for i in l1:
+            if i in l2:
+                res.remove(i)
+                l2.remove(i)
+        return NewList([i[0] for i in res])
+
+
+    def __isub__(self, other):
+        return self.__sub__(other)
+
+    def __rsub__(self, other):
+        return self - other
+
+    def __len__(self):
+        return len(self.new_list)
+
+    def check_type_other(self, other): #return NewList
+        if not isinstance(other, NewList):
+            return NewList(other)
+        return other
+
+    def get_list(self):
+        return self.new_list
+
+
+#Тесты:
+lst = NewList()
+lst1 = NewList([0, 1, -3.4, "abc", True])
+lst2 = NewList([1, 0, True])
+
+assert lst1.get_list() == [0, 1, -3.4, "abc", True] and lst.get_list() == [], "метод get_list вернул неверный список"
+
+res1 = lst1 - lst2 #[-3.4, 'abc']
+res2 = lst1 - [0, True]
+print(res2.get_list())
+res3 = [1, 2, 3, 4.5] - lst2
+lst1 -= lst2
+assert res1.get_list() == [-3.4, "abc"], "метод get_list вернул неверный список"
+assert res2.get_list() == [1, -3.4, "abc"], "метод get_list вернул неверный список"
+assert res3.get_list() == [2, 3, 4.5], "метод get_list вернул неверный список"
+assert lst1.get_list() == [-3.4, "abc"], "метод get_list вернул неверный список"
+
+lst_1 = NewList([1, 0, True, False, 5.0, True, 1, True, -7.87])
+lst_2 = NewList([10, True, False, True, 1, 7.87])
+res = lst_1 - lst_2
+print(res.new_list)
+assert res.get_list() == [0, 5.0, 1, True, -7.87], "метод get_list вернул неверный список"
+
+a = NewList([2, 3])
+res_4 = [1, 2, 2, 3] - a # NewList: [1, 2]
+assert res_4.get_list() == [1, 2], "метод get_list вернул неверный список"
