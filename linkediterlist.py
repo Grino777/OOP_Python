925256a66c8174643db40fcc7106d49e25bd2297
Grino777 <bars_B_P@mail.ru>
2022-12-15 14:54:26 +0300

    Finished linkediterlist.py

--- linkediterlist.py
+++ linkediterlist.py
@@ -12,57 +12,47 @@ class StackObj:
         self.__next = next
 
 
-
 class Stack:
     def __init__(self):
         self.top = None
-        self.length = 0
+        self._length = 0
 
     def __len__(self):
-        if self.top is None:
-            return 0
-        else:
-            temp = self.top
-            self.length = 1
-            while temp.next != None:
-                temp = temp.next
-                self.length += 1
+        return self._length
 
     def __getitem__(self, item):
-        return self.__iter__(item)
+        self.__indx_validator(item)
+        for v, i in enumerate(self, start=0):
+            if v == item:
+                return i.data
 
     def __setitem__(self, key, value):
-        ...
+        self.__indx_validator(key)
+        for v, i in enumerate(self):
+            if v == key:
+                i.data = value
 
     def __iter__(self):
         self.temp = self.top
         return self
 
     def __next__(self):
-        for i in range(self.length):
-            if not self.temp is None:
-                yield self.temp
-                self.temp = self.temp.next
-            else:
-                raise StopIteration
-
-    # def __iter__(self, indx=None):
-    #     if isinstance(indx, int):
-    #         temp = self.top
-    #         for i in range(self.length + 1):
-    #             if i == indx:
-    #                 return temp
-    #             else:
-    #                 temp = temp.next
-    #                 continue
-    #     else:
-    #         temp = self.top
-    #         for i in range(self.length):
-    #             yield temp
-    #             temp = temp.next
+        if not self.temp is None:
+            res = self.temp
+            self.temp = self.temp.next
+        else:
+            raise StopIteration
+        return res
 
     def push_back(self, obj):
         """Добавление нового объекта obj в конец стека"""
+        if self.top is None:
+            self.top = obj
+        else:
+            for v, i in enumerate(self, start=0):
+                if v == self._length - 1:
+                    i.next = obj
+        self._length += 1
 
     def push_front(self, obj):
         """Добавление нового объекта obj в начало стека"""
@@ -71,37 +61,30 @@ class Stack:
         else:
             obj.next = self.top
             self.top = obj
+        self._length += 1
+
+    def __indx_validator(self, indx):
+        if indx >= len(self):
+            raise IndexError('неверный индекс')
 
 
-
-    # def __indx_validator(self, indx):
-    #     if indx > len(self.length):
-    #         raise IndexError('неверный индекс')
-
+#Тесты:
 st = Stack()
-st.push_front(StackObj('1'))
-st.push_front(StackObj('2'))
+st.push_back(StackObj("1"))
+st.push_front(StackObj("2"))
 
-for i in st:
-    print(list(i))
+assert st[0] == "2" and st[1] == "1", "неверные значения данных из объектов стека, при обращении к ним по индексу"
 
-# print(list(st[1]))
+st[0] = "0"
+assert st[
+           0] == "0", "получено неверное значение из объекта стека, возможно, некорректно работает присваивание нового значения объекту стека"
 
-# st = Stack()
-# st.push_back(StackObj("1"))
-# st.push_front(StackObj("2"))
-#
-# assert st[0] == "2" and st[1] == "1", "неверные значения данных из объектов стека, при обращении к ним по индексу"
-#
-# st[0] = "0"
-# assert st[0] == "0", "получено неверное значение из объекта стека, возможно, некорректно работает присваивание нового значения объекту стека"
-#
-# for obj in st:
-#     assert isinstance(obj, StackObj), "при переборе стека через цикл должны возвращаться объекты класса StackObj"
-#
-# try:
-#     a = st[3]
-# except IndexError:
-#     assert True
-# else:
-#     assert False, "не сгенерировалось исключение IndexError"
+for obj in st:
+    assert isinstance(obj, StackObj), "при переборе стека через цикл должны возвращаться объекты класса StackObj"
+
+try:
+    a = st[3]
+except IndexError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение IndexError"
